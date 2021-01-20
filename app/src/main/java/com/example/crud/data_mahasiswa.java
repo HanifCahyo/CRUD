768e0e0dcfd8ef3657dcd4273aefd0c316b213e2
HanifCahyo <hanifcahyoprasetyo95@gmail.com>
2021-01-20 09:23:19 +0700

    first commit

---
+++ app/src/main/java/com/example/crud/data_mahasiswa.java
@@ -0,0 +1,61 @@
+package com.example.crud;
+
+public class data_mahasiswa {
+
+    //Deklarasi Variable
+    private String nim;
+    private String nama;
+    private String jurusan;
+    private String key;
+
+    //Membuat Konstuktor kosong untuk membaca data snapshot
+    public data_mahasiswa(){
+    }
+
+    //Konstruktor dengan beberapa parameter, untuk mendapatkan Input Data dari User
+    public data_mahasiswa(String nim, String nama, String jurusan) {
+        this.setNim(nim);
+        this.setNama(nama);
+        this.setJurusan(jurusan);
+    }
+
+    public String getNim() {
+        return nim;
+    }
+
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getJurusan() {
+        return jurusan;
+    }
+
+    public void setJurusan(String jurusan) {
+        this.jurusan = jurusan;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String toString() {
+        return " " + nim + "\n" +
+                " " + nama + "\n" +
+                " " + jurusan;
+    }
+
+}
+
